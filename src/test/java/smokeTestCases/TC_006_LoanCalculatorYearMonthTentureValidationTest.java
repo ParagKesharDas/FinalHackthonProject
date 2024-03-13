9b0b5331d93840640d58359352cddc4d7dd49c8a
Parag Keshar Das <70301818+ParagKesharDas@users.noreply.github.com>
2024-03-13 09:43:28 +0530

    Update TC_006_LoanCalculatorYearMonthTentureValidationTest.java

--- src/test/java/smokeTestCases/TC_006_LoanCalculatorYearMonthTentureValidationTest.java
+++ src/test/java/smokeTestCases/TC_006_LoanCalculatorYearMonthTentureValidationTest.java
@@ -30,7 +30,7 @@ public class TC_006_LoanCalculatorYearMonthTentureValidationTest extends BaseCla
 			String title = landingPage.getPageTitle();
 			System.out.println(title);
 			timeUnitSleep(2);
-			if (title.contains("Loan Calculator — Calculate EMI, Affordability, Tenure & Interest Rate")) {
+			if (title.equalsIgnoreCase("Loan Calculator — Calculate EMI, Affordability, Tenure & Interest Rate")) {
 				bp.logger.info("Page title matched");
 				System.out.println("Page title matched");
 				Assert.assertTrue(true);
